1997a6bcf9f244095095224e8a33b4e5e90d37f8
jeckCai <80797714@qq.com>
2019-09-16 17:15:52 +0800

    能提交吗？

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <!-- <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p> -->
     <h3>组件</h3>
     <ul>
       <li>
